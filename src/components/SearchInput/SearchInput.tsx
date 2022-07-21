ca7114bb97ab7b7b8e7488511599de5e5a35a621
Aris Suryono <aris.suryono.dev@gmail.com>
2022-07-21 22:03:01 +0700

    add on press enter

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -13,12 +13,17 @@ function SearchInput({ onSearch, query, setQuery }: SearchInputProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setQuery(e.target.value);
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSearch();
+  };
+
   return (
     <div className={styles.search__container}>
       <input
         className={styles.search__input}
         value={query}
         onChange={handleChange}
+        onKeyDown={handleKeyPress}
       />
       <button className={styles.search__submit} onClick={onSearch}>
         Search
